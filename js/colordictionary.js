7258c8a021558a09d544712021258545fe9aeeed
Warangkana Boontam <warangkanaboontam@gmail.com>
2017-08-23 10:38:45 +0700

    Restructure of project

--- js/colordictionary.js
+++
@@ -1,6 +0,0 @@
-var colorDict = {
-  "deposit": { "backgroundColor":"#26D0B4", "hoverBackgroundColor": "#64ECD5" },
-  "withdraw": { "backgroundColor":"#F7464A", "hoverBackgroundColor": "#FF5A5E" },
-  "transfer": { "backgroundColor":"#FDB45C", "hoverBackgroundColor": "#FFC870" },
-  "bill": { "backgroundColor":"#3B6BD3", "hoverBackgroundColor": "#628BE4" }
-};
