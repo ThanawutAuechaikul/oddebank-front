3d8551ea412868a8705c59bd62dfff308c65ca57
papon39946 <junior.papon@gmail.com>
2017-08-24 18:38:41 +0700

    refactor

--- app/js/bankUI.js
+++ app/js/bankUI.js
@@ -1,24 +1,5 @@
 var accountId = 5;
 
-
-function drawTable(data){
-    for (var i = 0; i < data.length; i++) {
-        drawRow(data[i]);
-    }
-}
-
-
-
-function drawRow(rowData){
-    var row = $("<tr />");
-    $('#transactionTable').append(row);
-    row.append($("<td>" + rowData.datetime + "</td>"));
-    row.append($("<td>" + rowData.transactionType + "</td>"));
-    row.append($("<td>" + rowData.amount + "</td>"));
-    row.append($("<td>" + rowData.balance + "</td>"));
-    row.append($("<td>" + rowData.remark + "</td>"));
-}
-
 function showNoTransaction(){
     $('#transactionTable').append($("<tr/> <td colspan='5' style='text-align: center;'><h4>  There is no transaction. </h4></td>"));
 }
@@ -27,7 +8,7 @@ function callGetAccountSummary(accountId){
     makeGETRequest('/account/summary/'+ accountId, '', drawAccountSummary);
 }
 
-function callGetChartData( chartElementId, chartType, accountId ){
+function buildChart( chartElementId, chartType, accountId ){
     makeGETRequest( '/dashboard/piechart/'+ accountId, '', function ( data ){ renderChart( chartElementId, chartType, data ) } );
 }
 
@@ -36,8 +17,8 @@ function buildDashboardPage( accountId ){
         var isShow = totalCountTransaction!=0;
         if( isShow )
 		{
-            renderTransactionHistory( totalCountTransaction, accountId );
-            callGetChartData( "pieChart", "pie", accountId );
+            buildTransactionHistoryTable( totalCountTransaction, accountId );
+            buildChart( "pieChart", "pie", accountId );
 		}else
 		{
             showNoTransaction();
--- test/js/transactionHistoryTest.js
+++ test/js/transactionHistoryTest.js
@@ -1,3 +1,6 @@
+
+
+
 QUnit.test( "Test callGetTransactionHistory", function(assert){
     var accountId = 5;
     var offset = 0;
@@ -31,10 +34,11 @@ QUnit.test( "Test renderTransactionHistory", function(assert){
     mockResponse('/dashboard/transaction/' + accountId +"?offset="+offset+"&limit="+limit, '{"transactionHistories":[{"id":"13","eventId":"13","transactionDate":1502415573000,"account":null,"transactionType":"BILL_PAYMENT","amount":500,"balance":20300,"remark":"Bill for electric"},{"id":"12","eventId":"12","transactionDate":1502327481000,"account":null,"transactionType":"DEPOSIT","amount":20000,"balance":20800,"remark":"Deposit cash"},{"id":"11","eventId":"11","transactionDate":1502247600000,"account":null,"transactionType":"TRANSFER","amount":200,"balance":800,"remark":"Transfer mock"},{"id":"10","eventId":"10","transactionDate":1502167173000,"account":null,"transactionType":"WITHDRAW","amount":700,"balance":1000,"remark":"Withdraw cash"},{"id":"9","eventId":"9","transactionDate":1502086833000,"account":null,"transactionType":"DEPOSIT","amount":800,"balance":1700,"remark":"Deposit cash"},{"id":"8","eventId":"8","transactionDate":1502026773000,"account":null,"transactionType":"WITHDRAW","amount":300,"balance":900,"remark":"Withdraw cash"},{"id":"7","eventId":"7","transactionDate":1501914753000,"account":null,"transactionType":"DEPOSIT","amount":1000,"balance":1200,"remark":"Deposit cash"},{"id":"6","eventId":"6","transactionDate":1501828053000,"account":null,"transactionType":"TRANSFER","amount":100,"balance":200,"remark":"Transfer mock"},{"id":"5","eventId":"5","transactionDate":1501749573000,"account":null,"transactionType":"WITHDRAW","amount":200,"balance":300,"remark":"Withdraw cash"},{"id":"4","eventId":"4","transactionDate":1501659573000,"account":null,"transactionType":"WITHDRAW","amount":100,"balance":500,"remark":"Withdraw cash"}],"total":11}');
     $('#qunit-fixture').append('<table id="transactionTable" class="table"> <thead class="mdb-color darken-3"></thead><tbody></tbody></table>');
 
-    renderTransactionHistory(totalTrans, accountId);
+    buildTransactionHistoryTable(totalTrans, accountId);
 
     assert.equal( $(".page-link:contains('5')")[0].innerText, expectedTotalPage, "Render transaction history");
-} )
+} );
+
 
 
 
