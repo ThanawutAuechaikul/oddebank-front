4ec6353a5c380ad26cb0ae61af0a7ee570be48ee
cadet <cadet@Geeky-Scrums-Mac-mini.local>
2017-08-24 11:10:38 +0700

    Add validate transfer js

--- app/js/transfer.js
+++ app/js/transfer.js
@@ -1,8 +1,9 @@
 makeGETRequest('user/' + '1' + '/accounts', '', loadAccountList);
-var fromAmount;
+var accounts;
 function loadAccountList(accountList) {
+    accounts = accountList;
     for (var i=0; i<accountList.length; i++) {
-        $('#fromAccount').append("<option value=" + accountList[i].id + ">" +
+        $('#fromAccount').append("<option value=" + i + ">" +
             formatAccountDisplay(accountList[i].accountNumber) + " (" + formatNumberDisplay(accountList[i].balance) +
             ")</option>");
     }
@@ -14,4 +15,18 @@ function formatNumberDisplay(x) {
 
 function formatAccountDisplay(x) {
     return x.replace(/(\d{3})(\d{3})(\d{3})(\d{1})/, "$1-$2-$3-$4");
+}
+
+
+function checkSubmitBtn(){
+
+    if($('#toAccount').val().length == 10 && $('#amount').val()>0 && $('#amount').val() <= accounts[$('#fromAccount').val()].balance){
+        $('#submitBtn').prop('disabled', false);
+    }else{
+        $('#submitBtn').prop('disabled', true);
+    }
+}
+
+function formatNumber(x){
+    x.value = parseFloat(x.value).toFixed(2);
 }
